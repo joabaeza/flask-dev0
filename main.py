e00b5e03861f30973f0e792ae0c7d4f8cf1db06c
joabaeza <joa.baeza.co@gmail.com>
2023-07-02 15:57:53 -0400

    inicial lenovo

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request,make_response, redirect, render_template
+
+
+app = Flask(__name__)
+
+todos = ['todo 1','todo 2','todo 3']
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip',user_ip)
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip' : user_ip,
+        'todos':todos
+    }
+    return render_template('hello.html', **context )
+    '''hello from platzi , tu ip es {}'.format(user_ip)'''
+
