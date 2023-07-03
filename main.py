aa4f3b9f981a52cbfb038e72fb9a03e4099b3b18
joaquin <joa@pop-os.localdomain>
2023-07-02 21:23:02 -0400

    clase 19

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, request,make_response, redirect, render_template,session
+from flask import Flask ,request,make_response, redirect, render_template,session,url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField,PasswordField
+from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 """
-17 4:28
+18 
 """
 
 
@@ -20,8 +20,9 @@ botstrap = Bootstrap(app)
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuarios')
-    password = PasswordField('CLave')
+    username = StringField('Nombre de usuarios', validators=[DataRequired()])
+    password = PasswordField('CLave', validators=[DataRequired()])
+    submit = SubmitField('enviar')
 
 
 
@@ -48,14 +49,24 @@ def index():
     """response.set_cookie('user_ip',user_ip)"""
     return response
 
-@app.route('/hello')
+
+@app.route('/hello',methods=['get','post'])
 def hello():
     """user_ip = request.cookies.get('user_ip')"""
     user_ip = session.get('user_ip')
+    login_form  = LoginForm()
+    username = session.get('username')
     context = {
         'user_ip' : user_ip,
-        'todos':todos
+        'todos':todos,
+        'login_form':login_form,
+        'username':username,
     }
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+        flash('nombre de usuario registrado')
+        redirect(url_for('index'))
     return render_template('hello.html', **context )
     '''hello from platzi , tu ip es {}'.format(user_ip)'''
 
